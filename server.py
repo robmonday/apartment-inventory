b06c9bc3dab5468d4b83e41d4e10520365be7868
Rob Monday <rob.monday@gmail.com>
2016-05-11 19:05:25 -0500

    Add templates that make use of extending

--- server.py
+++ server.py
@@ -5,19 +5,23 @@ app = Flask(__name__)
 @app.route('/all/')
 @app.route('/inventory/')
 def showAllInv():
-	return "Here is the entire inventory, all floorplans and all units."
+	# return "Here is the entire inventory, all floorplans and all units."
+	return render_template('index.html')
 
 @app.route('/floorplan/<floorplan_id>/')
 def showFloorplanInv(floorplan_id):
-	return "Here are all units for the %s floorplan." % (floorplan_id,)
+	# return "Here are all units for the %s floorplan." % (floorplan_id,)
+	return render_template('floorplan.html', floorplan_id = floorplan_id)
 
 @app.route('/floorplan/<floorplan_id>/unit/<unit_id>/')
 def showUnitDetail(floorplan_id, unit_id):
-	return "Here are specific details for unit %s, which has the %s floorplan." % (unit_id, floorplan_id)
+	# return "Here are specific details for unit %s, which has the %s floorplan." % (unit_id, floorplan_id)
+	return render_template('unit.html', floorplan_id = floorplan_id, unit_id = unit_id)
 
 @app.route('/floorplan/<floorplan_id>/unit/<unit_id>/edit/')
 def editUnit(floorplan_id, unit_id):
-	return "This URL allows for EDITING details about unit %s." % (unit_id,)
+	# return "This URL allows for EDITING details about unit %s." % (unit_id,)
+	return render_template('editunit.html', floorplan_id = floorplan_id, unit_id = unit_id)
 
 
 if __name__ == '__main__':
