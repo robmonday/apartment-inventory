7414de7fded85343b11e9661731f4fe0f1c071a4
Rob Monday <rob.monday@gmail.com>
2016-05-18 09:24:55 -0500

    changed data population to iterate through external JSON file to avoid DRY code

--- populate_data.py
+++ populate_data.py
@@ -7,6 +7,9 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+import json # import to enable reading JSON for unit info
+
+
 user1 = User(name='Unknown', email='Unknown')
 session.add(user1)
 session.commit()
@@ -31,66 +34,29 @@ loft = Floorplan(name='Loft', square_footage=695, bedrooms=1, bathrooms=1, user_
 session.add(loft)
 session.commit()
 
-two_bedroom1 = Floorplan(name='Cozy Two-Bedroom', square_footage=900, bedrooms=2, bathrooms=1, user_id=0)
+two_bedroom1 = Floorplan(name='Cozy Two-Bed', square_footage=900, bedrooms=2, bathrooms=1, user_id=0)
 session.add(two_bedroom1)
 session.commit()
 
-two_bedroom2 = Floorplan(name='Spacious Two-Bedroom', square_footage=1150, bedrooms=2, bathrooms=2, user_id=0)
+two_bedroom2 = Floorplan(name='Large Two-Bed', square_footage=1150, bedrooms=2, bathrooms=2, user_id=0)
 session.add(two_bedroom2)
 session.commit()
 
 print "Floorplan data populated successfully!"
 
-unit = Unit(name='5-101', floorplan_id=1, user_id=0)
-session.add(unit)
-session.commit()
+try:
+	with open('sample_data.json') as json_data:
+	    dataload = json.load(json_data)
+	    units = dataload['Unit']
+	    # print units
+	    iteration_num = 0
+	    for unit in units:
+	    	iteration_num += 1
+	    	print 'Add unit # '+str(iteration_num)
+	    	entry = Unit(name=unit['name'], floorplan_id=unit['floorplan_id'], user_id=unit['user_id'])
+	    	session.add(entry)
+	    	session.commit()
+	print "Unit data populated successfully!"
+except:
+	print "JSON did not load"
 
-unit = Unit(name='5-102', floorplan_id=2, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='5-103', floorplan_id=1, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='5-104', floorplan_id=2, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='5-105', floorplan_id=1, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='6-101', floorplan_id=3, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='6-102', floorplan_id=3, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='6-103', floorplan_id=3, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='6-104', floorplan_id=3, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='6-111', floorplan_id=4, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='6-112', floorplan_id=4, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='6-113', floorplan_id=4, user_id=0)
-session.add(unit)
-session.commit()
-
-unit = Unit(name='6-114', floorplan_id=4, user_id=0)
-session.add(unit)
-session.commit()
-
-print "Unit data populated successfully!"
